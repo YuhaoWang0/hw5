6b6dabca16e4b5283f2821b1b74b07148c11cec8
Yuhao Wang <ywang755@usc.edu>
2025-12-05 03:27:09 +0000

    HW5 final submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool backtrackSchedule(
+  const AvailabilityMatrix& avail, 
+  const size_t dailyNeed, 
+  const size_t maxShifts, 
+  DailySchedule& sched, 
+  std::vector<size_t>& shiftsUsed, 
+  size_t day, 
+  size_t slot
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -35,11 +43,77 @@ bool schedule(
     if(avail.size() == 0U){
         return false;
     }
-    sched.clear();
+
+    size_t nDays = avail.size();
+    size_t nWorkers = avail[0].size();
+
+    if(dailyNeed > nWorkers) {
+      return false;
+    }
+
+    if(nDays * dailyNeed > nWorkers * maxShifts) {
+      return false;
+    }
+
+    sched.assign(nDays, std::vector<Worker_T>());
+
+    std::vector<size_t> shiftsUsed(nWorkers, 0);
+
+    return backtrackSchedule(avail, dailyNeed, maxShifts, sched, shiftsUsed, 0, 0);
+
+
+
+    // sched.clear();
     // Add your code below
+}
 
 
+bool backtrackSchedule(
+  const AvailabilityMatrix& avail, 
+  const size_t dailyNeed, 
+  const size_t maxShifts, 
+  DailySchedule& sched, 
+  std::vector<size_t>& shiftsUsed, 
+  size_t day, 
+  size_t slot
+){
+  size_t nDays = avail.size();
+  size_t nWorkers = avail[0].size();
+
+  if(day == nDays) {
+    return true;
+  }
+
+  if(slot == dailyNeed) {
+    return backtrackSchedule(avail, dailyNeed, maxShifts, sched, shiftsUsed, day + 1, 0);
+  }
+
+  for(Worker_T w = 0; w < nWorkers; ++ w) {
+    if(!avail[day][w]){  // If not avaiable
+      continue;
+    }
+
+    if(shiftsUsed[w] >= maxShifts) {
+      continue;
+    }
+
+    // Already on the list
+    if(std::find(sched[day].begin(), sched[day].end(), w) != sched[day].end()){
+      continue;
+    }
+
+    sched[day].push_back(w);
+    shiftsUsed[w] ++;
+
+    if(backtrackSchedule(avail, dailyNeed, maxShifts, sched, shiftsUsed, day, slot + 1)){
+      return true;
+    }
+
+    shiftsUsed[w] --;
+    sched[day].pop_back();
 
 
+  }   
+  return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,14 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(
+  const std::string& in, 
+  std::string& current, 
+  const std::set<std::string>& dict, 
+  const std::set<std::string>& prefixes,
+  std::set<std::string>& results,
+  std::string floatingLeft, 
+  size_t index);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +29,84 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> results;
+    std::string current = in;
 
+    // Pruning
+    std::set<std::string> prefixes;
+    for(const std::string& w: dict) {
+      if(w.size() != in.size()) {
+        continue;
+      }
+      for(size_t len = 1; len <= w.size(); ++len) {
+        prefixes.insert(w.substr(0,len));
+      }
+    }
+
+    wordleHelper(in, current, dict, prefixes, results, floating, 0);
+
+    return results;
 }
 
 // Define any helper functions here
+void wordleHelper(
+  const std::string& in, 
+  std::string& current, 
+  const std::set<std::string>& dict, 
+  const std::set<std::string>& prefixes,
+  std::set<std::string>& results,
+  std::string floatingLeft, 
+  size_t index) {
+    // 1. Base Case
+    if(index == in.size()){
+      if(floatingLeft.empty() && dict.find(current) != dict.end()){
+        results.insert(current);
+      }
+      return;
+    }
+
+    // 1st Pruning  -- Not enough spaces.
+    int remaining = in.size() - index;
+    int spotLeft = static_cast<int>(remaining);
+    if(spotLeft < static_cast<int>(floatingLeft.size())) {
+      return;
+    }
+
+    if(index > 0) {
+      std::string prefix = current.substr(0, index);
+      if(prefixes.find(prefix) == prefixes.end()) {
+        return;
+      }
+    }
+
+    // Regular Case
+    if(in[index] != '-') {
+      current[index] = in[index];
+      wordleHelper(in, current, dict, prefixes, results, floatingLeft, index + 1);
+      return;
+    }
+
+    if(spotLeft == static_cast<int>(floatingLeft.size())) {
+      for(int i = 0; i < static_cast<int>(floatingLeft.size()); i ++) {
+        char c = floatingLeft[i];
+        current[index] = c;
+
+        std::string newFloating = floatingLeft;
+        newFloating.erase(i, 1);
+
+        wordleHelper(in, current, dict, prefixes, results, newFloating, index + 1);
+      }
+    } else {
+      for(char c = 'a'; c <='z'; c++) {
+        current[index] = c;
+
+        std::string newFloating = floatingLeft;
+        size_t pos = newFloating.find(c);
+        if(pos != std::string::npos){
+          newFloating.erase(pos, 1);
+        }
+
+        wordleHelper(in, current, dict, prefixes, results, newFloating, index + 1);
+       }
+    }
+  }
